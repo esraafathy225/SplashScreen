3a8d8a353d961b9e8e8f51e0d3dbecdd8e8e3312
esraa-fathy <esraa.fathy225@gmail.com>
2018-03-06 15:09:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/win7sp1/splashscreen/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.win7sp1.splashscreen;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+   RatingBar ratingBar;
+    TextView textView;
+    Button button1;
+    Button button2;
+    int progressState=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=(Button) findViewById(R.id.btn1);
+        button2=(Button) findViewById(R.id.btn2);
+        textView=(TextView) findViewById(R.id.txt);
+        //rating bar
+        ratingBar=(RatingBar) findViewById(R.id.bar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                textView.setText("Result is : "+rating);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProgressDialog();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog();
+            }
+        });
+    }
+
+    private void showProgressDialog() {
+        final ProgressDialog pDialog=new ProgressDialog(this);
+        pDialog.setMessage("Loading data ...");
+        pDialog.setCancelable(true);
+        pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pDialog.setProgress(0);
+        pDialog.setMax(100);
+        pDialog.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+            while(progressState<100){
+                progressState+=10;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                pDialog.setProgress(progressState);
+            }
+            if(progressState>=100)
+                pDialog.dismiss();
+            }
+        }).start();
+    }
+    private void createDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Are you sure to delete this item ? ")
+                .setMessage("Click me to confirm")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"item deleted",Toast.LENGTH_SHORT);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+    }
+}
